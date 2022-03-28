581ab7e9084a5ffb6b317528f59258a57efe0ad9
ankitpsamrat <ankit.p.samrat@gmail.com>
2022-03-28 08:20:20 +0530

    first commit

---
+++ array.java
@@ -0,0 +1,26 @@
+public class array {
+    public static void main(String[] args) {
+
+        // first method
+        // int num[];
+        // num = new int[5];
+
+        // second method
+        // int[] num;
+        // num=new int[5];
+
+        // third method
+        // int[] num = new int[5];
+        int num[] = new int[5];
+
+        num[0] = 10;
+        num[1] = 20;
+        num[2] = 30;
+        num[3] = 40;
+        num[4] = 50;
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+
+    }
+}
---
+++ array3.java
@@ -0,0 +1,8 @@
+public class array3 {
+    public static void main(String[] args) {
+        int num[] = { 10, 20, 30, 40, 50 };
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
