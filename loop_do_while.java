0275e7fdbb2404bcdda4ff1981093e9a05ba4abb
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-01-08 14:50:10 +0530

    do while loop

---
+++ loop_do_while.java
@@ -0,0 +1,10 @@
+public class loop_do_while {
+
+    public static void main(String[] args) {
+        int a = 0;
+        do {
+            System.out.println(a);
+            a=a+1;
+        } while (a < 11);
+    }
+}
