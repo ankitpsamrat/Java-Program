8109fd984388277b23d4048fa7a66a3a9e91ab0b
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-01-29 23:42:28 +0530

    function example

---
+++ funtion_exa3.java
@@ -0,0 +1,11 @@
+public class funtion_exa3 {
+    
+    // public static int multiplication(){
+
+    //     // return multi;
+    // }
+
+    public static void main(String[] args) {
+        
+    }
+}
