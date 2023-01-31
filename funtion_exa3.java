14cea12972acc4d93a30f495903940ec30194741
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-01-31 23:43:01 +0530

    function example

--- funtion_exa3.java
+++ funtion_exa3.java
@@ -1,11 +1,20 @@
-public class funtion_exa3 {
-    
-    // public static int multiplication(){
+import java.util.Scanner;
 
-    //     // return multi;
-    // }
+public class funtion_exa3 {
+
+    public static int multiplication(int a, int b) {
+        int multi = a * b;
+        return multi;
+    }
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first no : ");
+        int a = sc.nextInt();
+        System.out.print("Enter second no : ");
+        int b = sc.nextInt();
+        int mul = multiplication(a, b);
+        System.out.println("Multiplication is : " + mul);
+        sc.close();
     }
 }
