4985cba1ecb1a3cd3fc47e9103bd991f19b90666
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-09-11 23:23:03 +0530

    added

---
+++ array_3D_exa.java
@@ -0,0 +1,9 @@
+public class array_3D_exa {
+
+    public static void main(String[] args) {
+
+        int[][][] arr = { { { 1, 2 }, { 3, 4 } }, { { 5, 6 }, { 7, 8 } } };
+
+        System.out.println("arr[0][0][0] = " + arr[0][0][0]);
+    }
+}
