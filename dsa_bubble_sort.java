f0086691436f4f5422464245be74747d0af192e5
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-09-20 23:08:05 +0530

    added

---
+++ dsa_bubble_sort.java
@@ -0,0 +1,22 @@
+public class dsa_bubble_sort {
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        // bubble sort means: sort value in ascending order
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
