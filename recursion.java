cb269f9b26d393c7898a3813b22214c3736b57c4
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-09-25 23:07:33 +0530

    changes

--- recursion.java
+++ recursion.java
@@ -1,5 +1,8 @@
 public class recursion {
 
+    // The process in which a function calls itself directly or indirectly is called
+    // recursion and the corresponding function is called a recursive function.
+
     static int fact(int n) {
         if (n == 0 || n == 1) {
             return 1;
