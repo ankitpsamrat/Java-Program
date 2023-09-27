b54f4f9bca03060aae7f07b5fc4d56cde29cc4da
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-09-27 23:45:59 +0530

    changes

--- recursion2.java
+++ recursion2.java
@@ -1,17 +1,27 @@
 public class recursion2 {
-    // print 5 to 1
 
+    // print 5 to 1
+    // public static void num(int n) {
+    // if (n == 0) {
+    // return;
+    // } else {
+    // System.out.print(n + " ");
+    // num(n - 1);
+    // }
+    // }
+
+    // print 1 to 5
     public static void num(int n) {
-        if (n == 0) {
+        if (n == 6) {
             return;
         } else {
             System.out.print(n + " ");
-            num(n - 1);
+            num(n + 1);
         }
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1;
         num(n);
     }
 }
