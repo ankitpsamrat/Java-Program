24c3651da6056c57e878b592cfbd1fe20021d6ef
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-10-06 23:34:11 +0530

    added

---
+++ sort_insertion.java
@@ -0,0 +1,20 @@
+public class sort_insertion {
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 1, 2 };
+        int size = arr.length;
+
+        for (int i = 0; i < size; i++) {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && current < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ sort_selection.java
@@ -0,0 +1,25 @@
+public class sort_selection {
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 1, 2 };
+        int size = arr.length;
+
+        for (int i = 0; i < size - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            // swap
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
