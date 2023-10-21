c783bcda84576a5427d8245f192797e7bd526c6e
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-10-21 23:37:47 +0530

    added

---
+++ polymorphism_exa.java
@@ -0,0 +1,31 @@
+class Animal {
+    public void makeSound() {
+        System.out.println("Animal");
+    }
+}
+
+class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("Dog");
+    }
+}
+
+class Cat extends Animal {
+    public void makeSound() {
+        System.out.println("Cat");
+    }
+}
+
+public class polymorphism_exa {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.makeSound();
+
+        Dog dog = new Dog();
+        dog.makeSound();
+
+        Cat cat = new Cat();
+        cat.makeSound();
+    }
+}
