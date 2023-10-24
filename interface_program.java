5aec9d072a19637f6054f75571f70625bd0c9638
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-10-24 23:42:56 +0530

    changes

--- interface_program.java
+++ interface_program.java
@@ -1,17 +1,24 @@
-interface inter { // create class
+// An interface in Java is a blueprint of a class.
+// It has static constants and abstract methods.
+// abstract class is declared with the interface keyword.
 
-    void print();
+// interface class
+interface Animal {
+    public void walk();
 }
 
-class samrat implements inter { // create inherit class
-    public void print() {
-        System.out.println("Interface example");
+// create implement class
+class Horse implements Animal {
+    public void walk() {
+        System.out.println("Animal can walk");
     }
 }
 
 public class interface_program {
+
     public static void main(String[] args) {
-        samrat obj = new samrat(); // create object for inherit class
-        obj.print();
+        // create object for implement class
+        Horse obj = new Horse();
+        obj.walk();
     }
 }
