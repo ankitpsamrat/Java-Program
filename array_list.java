867595417982a4b7c5b1a92aea0a4291b9e7d1e8
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-11-28 22:51:27 +0530

    added

---
+++ array_list.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class array_list {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(0);
+        arr.add(1);
+        arr.add(2);
+        System.out.println(arr);
+    }
+}
