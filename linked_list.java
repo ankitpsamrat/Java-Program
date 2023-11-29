d33edd90247827ce1d283a5ff7a7e4d0eae559b2
ankitpsamrat <ankit.p.samrat@gmail.com>
2023-11-29 22:31:22 +0530

    added

---
+++ linked_list.java
@@ -0,0 +1,19 @@
+// singly linked list exam
+
+public class linked_list {
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        linked_list obj = new linked_list();
+
+    }
+}
