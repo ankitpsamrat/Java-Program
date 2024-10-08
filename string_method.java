789d1c5c37bda863417fd9c83543f3072b077e7e
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-10-08 23:06:23 +0530

    changes

--- string_method.java
+++ string_method.java
@@ -1,32 +1,49 @@
 public class string_method {
 
     public static void main(String[] args) {
+        String a = "sAmrAT";
 
         // toLowerCase method
-        String a = "SAMRAT";
         System.out.println(a.toLowerCase());
 
         // toUpperCase method
-        String b = "samrat";
-        System.out.println(b.toUpperCase());
+        System.out.println(a.toUpperCase());
 
         // length method
-        System.out.println(b.length());
+        System.out.println(a.length());
 
         // isEmpty method
-        System.out.println(b.isEmpty());
+        System.out.println(a.isEmpty());
 
         // trim method
-        String c = "  samrat   ";
+        String c = " samrat ";
         System.out.println(c.trim());
 
+        // startsWith method
+        System.out.println(a.startsWith("s"));
+
+        // endsWith method
+        System.out.println(a.endsWith("t"));
+
+        // equals method
+        System.out.println(a.equals(c));
+
         // charAt method
-        System.out.println(c.charAt(2));
+        System.out.println(a.charAt(2));
 
         // indexOf method
-        System.out.println(c.indexOf("m"));
+        System.out.println(a.indexOf("m"));
 
         // concat method
         System.out.println(a.concat(c));
+
+        // contains method
+        System.out.println(a.contains("A"));
+
+        // split method
+        String fullName = "Ankit Pratap Samrat";
+        for (String value : fullName.split(" ")) {
+            System.out.println(value);
+        }
     }
 }
