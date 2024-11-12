7bf94c1e293c73360f4c89bf0b806e2884e0b1e2
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-11-12 23:51:16 +0530

    added

---
+++ practice_string.java
@@ -0,0 +1,11 @@
+// Get a Character from a String
+
+public class practice_string {
+
+    public static void main(String[] args) {
+        String str = "Samrat";
+
+        char ch = str.charAt(3);
+        System.out.println(ch);
+    }
+}
