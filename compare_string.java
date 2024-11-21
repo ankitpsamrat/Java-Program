c9a586c2917f1ba162d073898a3eeae92e08bacb
ankitpsamrat <ankit.p.samrat@gmail.com>
2024-11-21 23:13:49 +0530

    added

---
+++ compare_string.java
@@ -0,0 +1,13 @@
+public class compare_string {
+
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello1";
+
+        if (str1 == str2) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+    }
+}
