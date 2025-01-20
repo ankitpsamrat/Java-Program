0057b71edf27925a10e312a45ae4d85c46812a25
ankitpsamrat <ankit.p.samrat@gmail.com>
2025-01-20 22:28:51 +0530

    added

---
+++ pattern.java
@@ -0,0 +1,15 @@
+// Print a pattern of * in a square shape.
+
+public class pattern {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
